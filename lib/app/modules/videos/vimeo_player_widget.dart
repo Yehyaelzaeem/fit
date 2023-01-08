b04f8579f119d6b6e752d6c9469436a6a7f8b69b
hussamm98 <hussam.zquad@gmail.com>
2023-01-08 16:59:23 +0200

    appbar vimeo

--- lib/app/modules/videos/vimeo_player_widget.dart
+++ lib/app/modules/videos/vimeo_player_widget.dart
@@ -1,4 +1,5 @@
 import 'package:app/app/modules/home/home_appbar.dart';
+import 'package:app/app/utils/helper/assets_path.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:vimeo_player_flutter/vimeo_player_flutter.dart';
@@ -13,7 +14,47 @@ final String link;
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            HomeAppbar(type: null),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 6),
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              width: MediaQuery.of(context).size.width,
+              height: 65,
+              decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(16), boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.4),
+                  blurRadius: 2,
+                  spreadRadius: 2,
+                  offset: Offset(0, 0),
+                ),
+              ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                      GestureDetector(
+                    onTap: () {
+                        Navigator.pop(context);
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      padding: EdgeInsets.all(4),
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        size: 30,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 10,
+                    child: Image.asset(
+                      kLogoRow,
+                      height: 54,
+                    ),
+                  ),
+               Expanded(child: SizedBox(),)
+                ],
+              ),
+            ),
             Expanded(
               child: VimeoPlayer(
                 videoId: link,
