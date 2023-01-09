c9888e21a67ec845081ca5a4355a58965c3b96cf
hussamm98 <hussam.zquad@gmail.com>
2023-01-09 16:01:53 +0200

    ios webview

--- lib/app/modules/cart/views/web_view.dart
+++ lib/app/modules/cart/views/web_view.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:io';
 
 import 'package:app/app/modules/home/home_appbar.dart';
 import 'package:app/app/modules/invoice/views/invoice_view.dart';
@@ -14,15 +15,11 @@ class WebViewScreen extends StatelessWidget {
   final int? packageId;
   final String? fromCheerfull;
 
-   WebViewScreen(
+  WebViewScreen(
       {Key? key, required this.url, this.packageId, this.fromCheerfull})
       : super(key: key);
 
-
-
-
-
- // late PullToRefreshController pullToRefreshController;
+  // late PullToRefreshController pullToRefreshController;
 
   InAppWebViewController? webViewController;
   InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
@@ -36,7 +33,6 @@ class WebViewScreen extends StatelessWidget {
       ios: IOSInAppWebViewOptions(
         allowsInlineMediaPlayback: true,
       ));
-
 
 /*  @override
   void initState() {
@@ -66,31 +62,80 @@ class WebViewScreen extends StatelessWidget {
               child: Center(
                 child: InAppWebView(
                   initialUrlRequest: URLRequest(url: Uri.parse(url)),
-                    initialUserScripts: UnmodifiableListView<UserScript>([]),
-                    onWebViewCreated: (myController) async {
+                  initialUserScripts: UnmodifiableListView<UserScript>([]),
+                  onWebViewCreated: (myController) async {
                     webViewController = myController;
                     print(await myController.getUrl());
                   },
-                    initialOptions: options,
-                 //   pullToRefreshController: pullToRefreshController,
-                    onLoadError: (ctrl, url, code, message) {
-                 //     pullToRefreshController.endRefreshing();
-                    },
-                  shouldOverrideUrlLoading: (controller, navigationAction) async {
+                  initialOptions: options,
+                  //   pullToRefreshController: pullToRefreshController,
+                  onLoadError: (ctrl, url, code, message) {
+                    //     pullToRefreshController.endRefreshing();
+                  },
+                  shouldOverrideUrlLoading:
+                      (controller, navigationAction) async {
                     final uri = navigationAction.request.url!;
                     print("uri = " + uri.toString());
-                    return NavigationActionPolicy.ALLOW;
-                  },
-                    onLoadStart: (webViewController, uri) {
-                    print("WEB URI link =========== > ${Uri.parse(uri.toString())}");
-                    print("on load onLoadStart ===>${uri!.path}");
-                    if (uri.path.contains("Success")) {
-                      if (fromCheerfull != "From Cheerful Order") {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (_) =>
-                                    InvoiceView(packageId:packageId!)));
+                    print("on ShouldOverrideUrlLoading ===>${uri.path}");
+                    if (Platform.isIOS) {
+                      if (uri.path == 'https://fofclinic.com/Payment-Success') {
+                        if (fromCheerfull != "From Cheerful Order") {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) =>
+                                      InvoiceView(packageId: packageId!)));
+                          Get.dialog(
+                            Dialog(
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SizedBox(height: 12),
+                                    Image.asset(
+                                      kSuccessful,
+                                      scale: 10,
+                                    ),
+                                    SizedBox(height: 12),
+                                    kTextbody("  Payment Successful  ",
+                                        color: Colors.black,
+                                        bold: true,
+                                        align: TextAlign.center),
+                                    SizedBox(height: 12),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        } else {
+                          Get.offNamed(Routes.ORDERS);
+                          Get.dialog(
+                            Dialog(
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    SizedBox(height: 12),
+                                    kTextbody(
+                                        "Thank you for ordering from Cheer-Full \n \n 😍 Have a cheerful day 😍",
+                                        color: Colors.black,
+                                        bold: true,
+                                        align: TextAlign.center),
+                                    SizedBox(height: 12),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                      } else if (uri.path.contains("Failed")) {
+                        print("Failed ${uri.path}");
                         Get.dialog(
                           Dialog(
                             child: Padding(
@@ -101,10 +146,55 @@ class WebViewScreen extends StatelessWidget {
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: [
                                   SizedBox(height: 12),
-                                  Image.asset(kSuccessful,scale: 10,),
+                                  Image.asset(
+                                    kFailed,
+                                    scale: 10,
+                                  ),
                                   SizedBox(height: 12),
-                                  kTextbody(
-                                      "  Payment Successful  ",
+                                  kTextbody("Payment Failed",
+                                      color: Colors.black,
+                                      bold: true,
+                                      align: TextAlign.center),
+                                  SizedBox(height: 12),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                    }
+                    return NavigationActionPolicy.ALLOW;
+                  },
+                  onLoadStart: (webViewController, uri) {
+                    print(
+                        "WEB URI link =========== > ${Uri.parse(uri.toString())}");
+                    print("on load onLoadStart ===>${uri!.path}");
+                    if (Platform.isIOS
+                        ? uri.path == 'https://fofclinic.com/Payment-Success'
+                        : uri.path.contains("Success") ||
+                            uri.path.contains("Payment-Success")) {
+                      if (fromCheerfull != "From Cheerful Order") {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) =>
+                                    InvoiceView(packageId: packageId!)));
+                        Get.dialog(
+                          Dialog(
+                            child: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  SizedBox(height: 12),
+                                  Image.asset(
+                                    kSuccessful,
+                                    scale: 10,
+                                  ),
+                                  SizedBox(height: 12),
+                                  kTextbody("  Payment Successful  ",
                                       color: Colors.black,
                                       bold: true,
                                       align: TextAlign.center),
@@ -138,7 +228,7 @@ class WebViewScreen extends StatelessWidget {
                           ),
                         );
                       }
-                    }else if (uri.path.contains("Failed")) {
+                    } else if (uri.path.contains("Failed")) {
                       print("Failed ${uri.path}");
                       Get.dialog(
                         Dialog(
@@ -150,10 +240,12 @@ class WebViewScreen extends StatelessWidget {
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: [
                                 SizedBox(height: 12),
-                                Image.asset(kFailed,scale: 10,),
+                                Image.asset(
+                                  kFailed,
+                                  scale: 10,
+                                ),
                                 SizedBox(height: 12),
-                                kTextbody(
-                                    "Payment Failed",
+                                kTextbody("Payment Failed",
                                     color: Colors.black,
                                     bold: true,
                                     align: TextAlign.center),
@@ -162,14 +254,14 @@ class WebViewScreen extends StatelessWidget {
                             ),
                           ),
                         ),
-                      );                     // Navigator.pop(context);
+                      );
                     }
                   },
-                    onLoadStop: (ctrl, url) async {
+              /*    onLoadStop: (ctrl, url) async {
                     print("on load stop ===>$url");
-               //     pullToRefreshController.endRefreshing();
-                    },
-               /*   onPageFinished: (String link) {
+                    //     pullToRefreshController.endRefreshing();
+                  },*/
+                  /*   onPageFinished: (String link) {
                     print("WEB link =========== > $link");
 
                   },*/
