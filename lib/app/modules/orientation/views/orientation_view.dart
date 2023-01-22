65ae4aba0e5bbb51654a532d01857d564b999d54
hussamm98 <hussam.zquad@gmail.com>
2023-01-22 15:52:56 +0200

    orientation title

--- lib/app/modules/orientation/views/orientation_view.dart
+++ lib/app/modules/orientation/views/orientation_view.dart
@@ -12,73 +12,90 @@ import 'package:get/get.dart';
 class OrientationView extends GetView<OrientationController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Obx(
-              () {
-            if (controller.loading.value)
-              return Center(child: CircularLoadingWidget());
-            if (controller.error.value.isNotEmpty)
-              return errorHandler(controller.error.value, controller);
+    return Scaffold(body: Obx(
+      () {
+        if (controller.loading.value)
+          return Center(child: CircularLoadingWidget());
+        if (controller.error.value.isNotEmpty)
+          return errorHandler(controller.error.value, controller);
 
-            return ListView(children: [
-              HomeAppbar(type: null),
-              SizedBox(height: 12),
-              Row(
-                children: [
-                  PageLable(name: "Orientation"),
-                ],
+        return ListView(children: [
+          HomeAppbar(type: null),
+          SizedBox(height: 12),
+          Row(
+            children: [
+              PageLable(name: "Orientation"),
+            ],
+          ),
+          SizedBox(height: 12),
+          GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 20,
+                childAspectRatio: 0.92,
+                mainAxisSpacing: 20,
               ),
-              SizedBox(height: 12),
-              GridView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  padding: EdgeInsets.symmetric(horizontal: 10),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 20,
-                    childAspectRatio: 0.92,
-                    mainAxisSpacing: 20,
-                  ),
-                  itemCount: controller.orientationVideosResponse.data?.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: EdgeInsets.all(4),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (_) => VimeoPlayerWidget(link:controller.orientationVideosResponse.data?[index].videoUrl??"")));
-                            },
+              itemCount: controller.orientationVideosResponse.data?.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: EdgeInsets.all(4),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => VimeoPlayerWidget(
+                                      link: controller.orientationVideosResponse
+                                              .data?[index].videoUrl ??
+                                          "")));
+                        },
+                        child: Container(
+                          height: MediaQuery.of(context).orientation ==
+                                  Orientation.landscape
+                              ? Get.height * 0.6
+                              : Get.height * 0.18,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(24),
+                              image: DecorationImage(
+                                  image: NetworkImage(controller
+                                          .orientationVideosResponse
+                                          .data?[index]
+                                          .image ??
+                                      ""),
+                                  fit: MediaQuery.of(context).orientation ==
+                                          Orientation.landscape
+                                      ? BoxFit.fill
+                                      : BoxFit.cover)),
+                          child: Center(
                             child: Container(
-                              height: Get.height * 0.18,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(24),
-                                  image: DecorationImage(
-                                      image: NetworkImage(controller.orientationVideosResponse.data?[index].image??""),
-                                      fit: BoxFit.cover)),
-                              child: Center(
-                                child: Container(
-                                    decoration: BoxDecoration(
-                                        shape: BoxShape.circle, color: Colors.white),
-                                    child: Icon(
-                                      Icons.play_circle,
-                                      color: kColorPrimary,
-                                    )),
-                              ),
-                            ),
+                                decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.white),
+                                child: Icon(
+                                  Icons.play_circle,
+                                  color: kColorPrimary,
+                                )),
                           ),
-                          Center(child: kTextbody(controller.orientationVideosResponse.data?[index].name??"",size: 12)),
-
-                        ],
+                        ),
                       ),
-                    );
-                  }),
-            ]);
-          },
-        ));
+                      Center(
+                          child: kTextbody(
+                              controller.orientationVideosResponse.data?[index]
+                                      .name ??
+                                  "",
+                              size: 12)),
+                    ],
+                  ),
+                );
+              }),
+        ]);
+      },
+    ));
   }
 }
